a003e2f395759232df6af8d3c006f51f6cb58268
leecrewml <46427943+leecrewml@users.noreply.github.com>
2019-03-04 18:55:48 -0800

    Robot program initial commit
    Robot program intial commit

---
+++ Intake/src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,38 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+
+/**
+ * Add your docs here.
+ */
+public class Intake extends Subsystem {
+  // Put methods for controlling this subsystem
+  Talon talon2 = null;
+  // here. Call these from Commands.
+public Intake(){
+  talon2 = new Talon(RobotMap.INTAKE_TALON_2);
+}
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+public void Out() {
+talon2.set(-0.75);
+}
+public void In() {
+  talon2.set(0.75);
+}
+public void Stop() {
+  talon2.set(0);
+}
+}
---
+++ Robot 2019/src/main/java/frc/robot/OI.java
@@ -0,0 +1,72 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.robot.commands.In;
+//import frc.robot.commands.Fall;
+//import frc.robot.commands.Rise;
+import frc.robot.commands.Out;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+  public static final String driveController = null;
+
+  public Joystick driverController = new Joystick(RobotMap.OI_DRIVE_CONTROLLER);
+
+Button button1 = new JoystickButton(driverController, 1);
+Button button4 = new JoystickButton(driverController, 4);
+Button button2 = new JoystickButton(driverController, 2);
+Button button3 = new JoystickButton(driverController, 3);
+  public OI() {
+    button4 = new JoystickButton(driverController, 4);
+    //button4.whileHeld(new Rise());
+    button1 = new JoystickButton(driverController, 1);
+    //button1.whileHeld(new Fall());
+    button2 = new JoystickButton(driverController, 2);
+    button2.whileHeld(new In());
+    button3 = new JoystickButton(driverController, 3);
+    button3.whileHeld(new Out());
+
+
+
+  //// CREATING BUTTONS
+  // One type of button is a joystick button which is any button on a
+  //// joystick.
+  // You create one by telling it which joystick it's on and which button
+  // number it is.
+  // Joystick stick = new Joystick(port);
+
+  }
+
+  // There are a few additional built in buttons you can use. Additionally,
+  // by subclassing Button you can create custom triggers and bind those to
+  // commands the same as any other Button.
+
+  //// TRIGGERING COMMANDS WITH BUTTONS
+  // Once you have a button, it's trivial to bind it to a button in one of
+  // three ways:
+
+  // Start the command when the button is pressed and let it run the command
+  // until it is finished as determined by it's isFinished method.
+  // button.whenPressed(new ExampleCommand());
+
+  // Run the command while the button is being held down and interrupt it once
+  // the button is released.
+  // button.whileHeld(new ExampleCommand());
+
+  // Start the command when the button is released and let it run the command
+  // until it is finished as determined by it's isFinished method.
+  // button.whenReleased(new ExampleCommand());
+}
+
